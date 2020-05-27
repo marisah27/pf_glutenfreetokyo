7163674773d4980f42da252ba6daa6cc3b2eb7fa
marisa.h <m.s.2c.1226@gmail.com>
2020-05-27 12:28:51 +0000

    add shopspostview

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,2 +1,44 @@
 class ShopsController < ApplicationController
+  before_action :require_user_logged_in
+  
+  def index
+    @shops = current_user.shops.order(id: :desc).page(params[:page])
+  end
+
+  def show
+  end
+
+  def new
+    @shop = current_user.shops.build
+  end
+
+  def create
+    @shops = current_user.shops.build(shop_params)
+    if @shops.save
+      flash[:success] = 'お店情報を投稿しました。'
+      redirect_to root_url
+    else
+      @shops = current_user.shops.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'お店情報の投稿に失敗しました。'
+      render 'toppages/index'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  
+  # Strong Parameter
+  def shop_params
+    params.require(:shop).permit(:name, :address, :review, :area, :genre, :img)
+  end
+  
 end
+
