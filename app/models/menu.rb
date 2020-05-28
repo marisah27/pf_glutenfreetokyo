aa543f185605c5ba4824c61a04b98b7f42d5f07f
marisa.h <m.s.2c.1226@gmail.com>
2020-05-28 14:34:30 +0000

    add menu validation

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,7 @@
 class Menu < ApplicationRecord
   belongs_to :shop
+  mount_uploader :img, ImgUploader
+  
+  validates :menu, length: { maximum: 100 }
+  validates :price, allow_blank: true,numericality: { only_integer: true }
 end
